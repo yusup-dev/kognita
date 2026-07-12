60a4fe8fd012c5fe9770317abf424850704d0f06
Muhamad Yusup <muh.yusup965@gmail.com>
2026-07-13 03:00:33 +0700

    initial commit

---
+++ src/main/java/com/kognita/KognitaApplication.java
@@ -0,0 +1,13 @@
+package com.kognita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KognitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KognitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kognita/KognitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kognita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KognitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
